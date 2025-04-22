15cd4d073d2f84f463fcdf0394cb7fee3be92ff5
ARYAN TECHIE <76107940+Aryan-Techie@users.noreply.github.com>
2025-04-22 20:37:14 +0530

    Quartz sync: Apr 22, 2025, 8:37 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://aryan-techie.github.io/insight",
+    baseUrl: "https://insight.aroice.in",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
